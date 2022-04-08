8ecbcc6fc15f363d983ed8f2e22b095697245150
HoangThoang <thoanghv@vmodev.com>
2022-04-08 15:15:14 +0700

    update home-page footer-page

--- plant-store/src/app/app.module.ts
+++ plant-store/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomePageComponent } from './home-page/home-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- plant-store/src/app/footer/footer.component.ts
+++ plant-store/src/app/footer/footer.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+declare const google: any;
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
+  map: any;
+
+  @ViewChild('mapElement') mapElement: any;
+
+
+
   ngOnInit(): void {
+
   }
 
+
+  ngAfterViewInit(): void {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+    center: { lat: 21.0312695, lng: 105.7818161 },
+    zoom: 18,
+    });
+
+   const marker: any = new google.maps.Marker({
+    position: { lat: 21.0312695, lng: 105.7818161 },
+    map: Map,
+    });
+
+
+   const infowindow = new google.maps.InfoWindow({
+    content: "<p>Marker Location:" + marker.getPosition() + "</p>",
+    });
+
+   google.maps.event.addListener(marker, "click", () => {
+    infowindow.open(this.map, marker);
+    });
+  }
 }

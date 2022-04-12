822f60ff41c9078150c1fc2dae031cd7479d9887
HoangThoang <thoanghv@vmodev.com>
2022-04-12 13:48:57 +0700

    update

--- plant-store/src/app/home-page/home-page.component.ts
+++ plant-store/src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
